77c1f875420b7acba6cacfe5974b8aba37b349b9
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2024-12-17 04:50:38 -0500

    Added logic to vimtex plugin spec (nvim) Added autocmds for gutentags for more controlled tag generation (nvim)

--- nvim/.config/nvim/lua/plugins/general/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/general/vimtex.lua
@@ -5,12 +5,11 @@ local M = {
     lazy = false,
     init = function()
       vim.g.vimtex_view_method = "sioyek"
-      vim.g.matchup_override_vimtex = 1
+      if require("plugins.config.has-c-compiler") then
+        vim.g.matchup_override_vimtex = 1
+      end
     end,
   },
-
-  -- Generates tag files. (vimTeX requested)
-  -- 'ludovicchabant/vim-gutentags',
 
   -- Manipulate surrounding enclosions like (), [], <p> (vimTeX requested)
   "tpope/vim-surround",
@@ -19,6 +18,32 @@ local M = {
 if require("plugins.config.has-c-compiler") then
   -- Extends Vim's surrounding enclosions for language specific surroundings (vimTeX requested)
   table.insert(M, "andymass/vim-matchup")
+end
+
+if vim.fn.executable("ctags") == 1 then
+  -- Generate tags for the current project (Allows for jumping to definitions)
+  table.insert(M, {
+    "ludovicchabant/vim-gutentags",
+    config = function()
+      vim.g.gutentags_generate_on_write = 0
+      vim.g.gutentags_generate_on_new = 0
+      vim.g.gutentags_generate_on_missing = 0
+      vim.g.gutentags_generate_on_empty_buffer = 0
+
+      -- Use autocmds and :GutentagsUpdate to generate tags instead
+      -- This way tags are only generated for LaTeX files
+      local gutentags_group = vim.api.nvim_create_augroup("gutentags-latex", { clear = true })
+      vim.api.nvim_create_autocmd(
+        -- Generate tags when a new LaTeX file is created, opened, or saved
+        { "BufNewFile", "BufRead", "BufWritePost" },
+        {
+          pattern = "*.tex",
+          group = gutentags_group,
+          command = "GutentagsUpdate",
+        }
+      )
+    end,
+  })
 end
 
 return M
