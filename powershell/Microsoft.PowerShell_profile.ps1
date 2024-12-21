1dbb35b5b9e33575f84e889b1af047a63a4db44b
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2024-12-20 21:07:04 -0500

    Add ohmyposh configuration

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,13 @@
+# Set OHMYPOSH to the path of the oh-my-posh config file manually if automatic resolution is wished to be bypassed.
+$OHMYPOSH = ""
+
+$PROFILE_ITEM = Get-Item $PSCommandPath
+if (($PROFILE_ITEM.Attributes -band [System.IO.FileAttributes]::ReparsePoint) -and ($OHMYPOSH -eq "")) {
+  # Resolve the symlink to the actual path and navigate to OMP config
+  $target_directory = $PROFILE_ITEM.Target | Split-Path
+  $OHMYPOSH = Join-Path $target_directory "..\ohmyposh\.config\ohmyposh\config.toml" | Resolve-Path
+}
+
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
@@ -12,4 +22,20 @@ if (Test-Path($ChocolateyProfile)) {
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
   Invoke-Expression (& { (zoxide init powershell | Out-String) })
   Set-Alias cd z -Option AllScope
+}
+
+# # Disable OMP for now
+# # If $POSH_INIT is not set, then initialize oh-my-posh
+# if ((Get-Command oh-my-posh -ErrorAction SilentlyContinue) -and (-not $POSH_INIT)) {
+#   $POSH_INIT = $true
+#   if (Test-Path $OHMYPOSH) {
+#     oh-my-posh init pwsh --config $OHMYPOSH | Invoke-Expression
+#   } else {
+#     Write-Host "No oh-my-posh config file found. Using default config."
+#     oh-my-posh init pwsh | Invoke-Expression
+#   }
+# }
+
+function Prompt {
+  "`n$PWD`nPS `$ "
 }
