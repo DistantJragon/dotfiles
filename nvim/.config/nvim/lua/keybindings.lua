1b1b5ffbcfc9fdbd8d37d31bf8be9388e43c9aed
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2024-12-28 01:22:16 -0500

    Update some keybindings to use which-key (nvim)

--- nvim/.config/nvim/lua/keybindings.lua
+++ nvim/.config/nvim/lua/keybindings.lua
@@ -1,8 +1,22 @@
+local which_key = require("which-key")
 -- Run keybindings-debug-mode.lua
 require("keybindings-debug-mode")
 
 -- Exit terminal mode with escape
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
+-- vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
+which_key.add({
+  { "<Esc>", "<C-\\><C-n>", mode = "t", desc = "Exit terminal mode with escape" },
+})
 
 -- Remove search highlighting with escape
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+-- vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+which_key.add({
+  {
+    "<Esc>",
+    function()
+      vim.opt.hlsearch = false
+    end,
+    mode = "n",
+    desc = "Remove search highlighting with escape",
+  },
+})
