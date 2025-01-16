e46a1d671b180e6eb1aa0c913cd37f1086f7321b
Jacob Goodwin <jacobgoodwin2017@gmail.com>
2025-01-16 15:04:08 -0500

    Rearrange Powershell profile so zoxide works correctly

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -10,17 +10,9 @@ if (($PROFILE_ITEM.Attributes -band [System.IO.FileAttributes]::ReparsePoint) -a
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
-}
-
-# Set up zoxide
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-  Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 }
 
 # # Disable OMP for now
@@ -42,4 +34,9 @@ if (Get-Command fnm -ErrorAction SilentlyContinue) {
 
 function Prompt {
   "`n$PWD`nPS `$ "
+}
+
+# Set up zoxide
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+  Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 }
