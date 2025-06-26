04053fdf29e4eb4a773b85c36cf2874b149bd239
Jacob Goodwin <jacobgoodwin2017@gmail.com>
2025-06-26 17:04:49 -0400

    Custom Powershell variables storing locations

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 $djnscripts = "D:\MyFiles\Scripts"
 $djnrepos = "D:\MyFiles\repos"
-$djnpython = "C:\Program Files\Python"
+$djnpython = "C:\Users\jacob\AppData\Local\Programs\Python"
 
 # Set OHMYPOSH to the path of the oh-my-posh config file manually if automatic resolution is wished to be bypassed.
 $OHMYPOSH = ""
